4942367513dfab0f13e3144a92deed63ea2d870b
Duccio Rocchini <ducciorocchini@gmail.com>
2023-09-07 13:36:30 +0200

    Create students.r

---
+++ students.r
@@ -0,0 +1,9 @@
+# giulian capasso
+viridis <- colorRampPalette(viridis(7))(255)
+
+# maria alberti
+cl <- colorRampPalette(brewer.pal(11, 'Spectral'))(100)
+plot(c18, col=cl)+
+  title("chilean volcano 2018", line = 10)
+
+
