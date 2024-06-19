7a44ae718c3fa3d1626bf018e2ad094de5be1a5b
Duccio Rocchini <ducciorocchini@gmail.com>
2024-06-19 14:48:52 +0200

    Create classes_names_classification.R

---
+++ classes_names_classification.R
@@ -0,0 +1,11 @@
+Filippo  Paganelli 
+
+# class 1 = vegetazione
+# class 2 = suolo nudo
+nomi_classi <- c("Vegetazione", "Suolo nudo") # uguali per entrambi in questo caso
+
+# Le osservo una sopra all'altra
+# Le plotto dando anche un nome alle classi in maniera da essere più chiaro
+VB <- colorRampPalette(c("forestgreen", "floralwhite"))(2) # creo la mia scala di colori con solo 2 sfumature
+par(mfrow=c(2,1)) # 2 righe e 1 colonna
+plot(m23c, main = "Classificazione marzo 23", type = "classes", levels = nomi_classi, col = VB)
