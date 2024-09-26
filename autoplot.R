06ccb4eae20f31ec1ea4eae138a5067fe652a45b
Duccio Rocchini <ducciorocchini@gmail.com>
2024-09-26 15:32:33 +0200

    Update autoplot.R

--- autoplot.R
+++ autoplot.R
@@ -1,6 +1,10 @@
 # Civenti Mattia 30
 # github: MattiaCiventi
 
+# per facilitare la visualizzazione di analisi statistiche multivariate
+library(ggfortify)
+library(factoextra)
+
 # Crea un biplot per visualizzare la PCA
 autoplot(pca_result, data = data, colour = 'Anno', loadings = TRUE, loadings.label = TRUE) +
   labs(title = "Biplot dell'Analisi delle Componenti Principali (PCA)",
