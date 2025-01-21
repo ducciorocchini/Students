e8a46d2aa931087cacf3d5c2ca7c2677a576d9be
Duccio Rocchini <ducciorocchini@gmail.com>
2025-01-21 14:24:49 +0100

    Create X11_new_device.R

---
+++ X11_new_device.R
@@ -0,0 +1,5 @@
+# From Matteo Marcantonio
+
+X11()
+
+# On Mac OS X if you use RStudio or R from the terminal, you must have a copy of XQuartz, the X11 window manager, installed. This is no longer a default install since Mac OS X 10.8. You need to install XQuartz, before installing the nat package. Logout and login for the installation to take effect. You can get it from https://www.xquartz.org/. This page is also linked from the Download R for (Mac) OS X page.
